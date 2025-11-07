b7545783ccca52b04e4e278b2125c372ed85fbda
385944 <a385944@uach.mx>
2025-11-07 14:53:38 -0600

    Update appy.py

--- appy.py
+++ appy.py
@@ -23,3 +23,41 @@ if st.button("Calcular IMC"):
     
     # Mostrar resultado
     st.subheader(f"Tu IMC: {imc:.1f}")
+
+if imc < 18.5:
+        st.warning("Categoría: Bajo peso")
+        st.info("""
+        **Consejos:**
+        • Aumenta tus porciones poco a poco. Tu cuerpo necesita más combustible.
+        • Incluye alimentos nutritivos como frutos secos y aguacate.
+        • Realiza ejercicio de fuerza.
+        • Agrega proteína en cada comida.
+        """)
+
+elif imc < 24.9:
+    st.writing("categoría: peso normal")
+    st.info("""
+    **consejos:**
+    • ¡Excelente! Mantén tus hábitos saludables. 
+    • Hidrátate y duerme bien. Eso marca la diferencia.
+    • Pequeños hábitos diarios = grandes resultados a largo plazo.
+    • COntinua con ejercicio de forma regular.
+
+elif imc < 29.9:
+    st.writing("categoría: sobre peso")
+    st.info("""
+    **consejos:**
+    • Comienza con pasos simples: más agua, menos bebidas azucaradas.
+    • Camina y has ejercico 30 minutos diarios.
+    • Las verduras deben cubrir 1/3 de tu plato.
+    • Controla el tamaño de las porciones.
+
+else:
+    st.writing("categoría: obesidad")
+    st.info("""
+    **consejos:**
+    • Consulta con un profesional de la salud. 
+    • No se trata de rapidez, sino de constancia. Empieza hoy.
+    • EStablece metas pequeñas y alcansables.
+    • Comienza con ejercicios suabes como la caminata o la natación.
+    
