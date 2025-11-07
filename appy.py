3b546d6cd24117533f5ce011d91caeecfd952608
385944 <a385944@uach.mx>
2025-11-07 14:28:51 -0600

    Update appy.py

--- appy.py
+++ appy.py
@@ -2,3 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+st.title("⚖️ Calculadora de IMC")
+st.write("Calcula tu Índice de Masa Corporal y recibe consejos personalizados")
