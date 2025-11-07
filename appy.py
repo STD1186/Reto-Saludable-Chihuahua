afe61bd1e5c0be4a054ff4459436d67cd2f55595
385944 <a385944@uach.mx>
2025-11-07 14:33:22 -0600

    Update appy.py

--- appy.py
+++ appy.py
@@ -11,6 +11,6 @@ st.subheader("Tus datos")
 
 altura = st.slider("Altura (cm)", 100, 220, 170)
 peso = st.slider("Peso (kg)", 40, 150, 70)
-edad = st.slider("Edad", 18, 100, 30)
+edad = st.slider("Edad", 0, 100, 30)
 
 genero = st.radio("Género", ["Masculino", "Femenino", "Otro"])
