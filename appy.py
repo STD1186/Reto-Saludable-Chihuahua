b27d9c8650db64dbbfccb144c83e3786f85e3733
385944 <a385944@uach.mx>
2025-11-07 14:32:00 -0600

    Update appy.py

--- appy.py
+++ appy.py
@@ -5,3 +5,12 @@ import numpy as np
 
 st.title("⚖️ Calculadora de IMC")
 st.write("Calcula tu Índice de Masa Corporal y recibe consejos personalizados")
+
+#datos
+st.subheader("Tus datos")
+
+altura = st.slider("Altura (cm)", 100, 220, 170)
+peso = st.slider("Peso (kg)", 40, 150, 70)
+edad = st.slider("Edad", 18, 100, 30)
+
+genero = st.radio("Género", ["Masculino", "Femenino", "Otro"])
