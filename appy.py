8b2e0803ed70ee8bc6bef3e8e38428ab61574ec2
385944 <a385944@uach.mx>
2025-11-07 14:34:07 -0600

    Update appy.py

--- appy.py
+++ appy.py
@@ -9,8 +9,8 @@ st.write("Calcula tu Índice de Masa Corporal y recibe consejos personalizados")
 #datos
 st.subheader("Tus datos")
 
-altura = st.slider("Altura (cm)", 100, 220, 170)
-peso = st.slider("Peso (kg)", 40, 150, 70)
+altura = st.slider("Altura (cm)", 0, 220, 170)
+peso = st.slider("Peso (kg)", 0, 150, 70)
 edad = st.slider("Edad", 0, 100, 30)
 
 genero = st.radio("Género", ["Masculino", "Femenino", "Otro"])
