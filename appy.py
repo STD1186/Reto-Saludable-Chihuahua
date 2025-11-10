a193a71e6dd643e35371bf397a61e80afdd8024d
385944 <a385944@uach.mx>
2025-11-10 00:24:17 -0600

    Update appy.py

--- appy.py
+++ appy.py
@@ -29,7 +29,7 @@ st.subheader("🏅 Tu Meta Personal")
 
 meta = st.radio(
     "¿Cuál es tu objetivo?",
-    ["Bajar peso", "Mantener peso", "Ganar masa muscular"],
+    ["⬇️Bajar peso", "⚖️ Mantener peso", "💪Ganar masa muscular"],
     horizontal=True
 )
 
